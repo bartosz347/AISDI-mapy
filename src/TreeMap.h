c6ef210360685ed8387d901ec1418fd8c2dfb4cd
Bartosz <bartosz@bartosz-pc>
2016-12-03 22:21:28 +0100

    tree implementation pt4

--- src/TreeMap.h
+++ src/TreeMap.h
@@ -32,21 +32,21 @@ private: // TODO move to lower private section
     BinaryNode *left;
     BinaryNode *right;
     BinaryNode *parent;
-    //key_type key;
-    //mapped_type value;
+
     //value_type data; TODO how to use const key_type?
     std::pair<key_type, mapped_type> data;
     //BinaryNode(BinaryNode *left, BinaryNode *right, BinaryNode *parent) : left(left), right(right), parent(parent) {} // todo is used?
     BinaryNode() {}
   };
-  BinaryNode *head;
+  BinaryNode *head; // super head
   size_type size;
 
   void setup()
   {
     head = new BinaryNode();
-    head->left = head;
-    head->right = head;
+    head->left = head; //todo
+    head->right = head; // used for detecting illegal --begin() with empty collection
+    head->parent = nullptr;
     size = 0;
   }
 
@@ -59,32 +59,33 @@ public:
 
   TreeMap(std::initializer_list<value_type> list)
   {
-    (void)list; // disables "unused argument" warning, can be removed when method is implemented.
-    throw std::runtime_error("TODO");
+    setup();
+    for(auto element : list)
+      operator[](element.first) = element.second;
   }
 
   TreeMap(const TreeMap& other)
   {
     (void)other;
-    throw std::runtime_error("TODO");
+    throw std::runtime_error("TODOs");
   }
 
   TreeMap(TreeMap&& other)
   {
     (void)other;
-    throw std::runtime_error("TODO");
+    throw std::runtime_error("TODOi");
   }
 
   TreeMap& operator=(const TreeMap& other)
   {
     (void)other;
-    throw std::runtime_error("TODO");
+    throw std::runtime_error("TODOzz");
   }
 
   TreeMap& operator=(TreeMap&& other)
   {
     (void)other;
-    throw std::runtime_error("TODO");
+    throw std::runtime_error("TODOzzz");
   }
 
   bool isEmpty() const
@@ -94,78 +95,142 @@ public:
 
   mapped_type& operator[](const key_type& key)
   {
-    (void)key;
-    if(size == 0) {
-        head->left = new BinaryNode;
-        head->left->parent = head;
-        head->left->left = nullptr;
-        head->left->right = nullptr;
-        head->data = std::make_pair(key, mapped_type{});
-        head->right = nullptr;
+    BinaryNode *newNode = new BinaryNode();
+    newNode->right = nullptr;
+    newNode->left = nullptr;
+    newNode->data = std::make_pair(key, mapped_type{});
+    if(isEmpty()) {
+        newNode->parent = head;
+
+
+        head->left = newNode; // list no longer empty
+
+        head->right = nullptr; // important for check against decrementing begin() in empty map
         size++;
-        return head->data.second;
+        return newNode->data.second;
     }
-    // TODO implement inserting on ther pos than head
-    //BinaryNode *newNode = new BinaryNode(nullptr,nullptr);
-    throw std::runtime_error("TODO");
+    BinaryNode *next = head->left;
+    BinaryNode *current;
+    while(next != nullptr) {
+        current = next;
+        if(key == current->data.first) { //node with this key already exists
+            delete newNode;
+            return current->data.second;
+        }
+        if(key < current->data.first)
+            next = current->left;
+        else
+            next = current->right;
+    }
+    newNode->parent = current;
+    if(key < current->data.first)
+      current->left = newNode;
+    else
+      current->right = newNode;
+    size++;
+    return newNode->data.second;
   }
 
   const mapped_type& valueOf(const key_type& key) const
   {
-    (void)key;
-    throw std::runtime_error("TODO");
+    if(isEmpty())
+      throw std::out_of_range("map is empty");
+    const_iterator position = find(key);
+    if(position == cend())
+      throw std::out_of_range("key does not exist");
+    return position->second;
   }
 
   mapped_type& valueOf(const key_type& key)
   {
-    (void)key;
-    throw std::runtime_error("TODO");
+    if(isEmpty())
+      throw std::out_of_range("map is empty");
+    iterator position = find(key);
+    if(position == end())
+      throw std::out_of_range("key does not exist");
+    return position->second;
   }
 
   const_iterator find(const key_type& key) const
   {
-    (void)key;
-    throw std::runtime_error("TODO");
+    if(head == head->left || size == 0)
+      return cend();
+    BinaryNode *current = head->left;
+
+    while(current != nullptr) {
+        if(key == current->data.first)
+            return const_iterator(current);
+        if(key < current->data.first)
+            current = current->left;
+        else
+            current = current->right;
+    }
+    return cend();
   }
 
-  iterator find(const key_type& key)
+  iterator find(const key_type& key) // todo combine with const_iterator find
   {
-    (void)key;
-    throw std::runtime_error("TODO");
+   if(head == head->left || size == 0)
+      return end();
+    BinaryNode *current = head->left;
+
+    while(current != nullptr) {
+        if(key == current->data.first)
+            return const_iterator(current);
+        if(key < current->data.first)
+            current = current->left;
+        else
+            current = current->right;
+    }
+    return end();
   }
 
   void remove(const key_type& key)
   {
     (void)key;
-    throw std::runtime_error("TODO");
+    throw std::runtime_error("TODOa3");
   }
 
   void remove(const const_iterator& it)
   {
     (void)it;
-    throw std::runtime_error("TODO");
+    throw std::runtime_error("TODO2");
   }
 
   size_type getSize() const
   {
-    throw std::runtime_error("TODO");
+    return size;
   }
 
-  bool operator==(const TreeMap& other) const
+  bool operator==(const TreeMap& other) const // todo is there a better way?
   {
-    (void)other;
-    throw std::runtime_error("TODO");
+    if(size != other.size)
+      return false;
+    auto otherIt = other.cbegin();
+    auto ownIt = cbegin();
+
+    for(size_type i = 0; i < size; i++)
+    {
+        if(!(otherIt->first == ownIt->first && otherIt->second == ownIt->second))
+            return false;
+        otherIt++;
+        ownIt++;
+    }
+    return true;
   }
 
   bool operator!=(const TreeMap& other) const
   {
-    (void)other;
-    throw std::runtime_error("TODO");
+    return !operator==(other);
   }
 
-  iterator begin()
+  iterator begin() // TODO combine with cbegin() ?
   {
-    return ConstIterator(head->left);
+    BinaryNode *leftMostNode = head;
+    if(head == head->left) return ConstIterator(head); // empty map
+    while(leftMostNode->left != nullptr)
+        leftMostNode = leftMostNode->left;
+    return ConstIterator(leftMostNode);
   }
 
   iterator end()
@@ -175,7 +240,11 @@ public:
 
   const_iterator cbegin() const
   {
-    return ConstIterator(head->left);
+    BinaryNode *leftMostNode = head;
+    if(head == head->left) return ConstIterator(head); // empty map
+    while(leftMostNode->left != nullptr)
+        leftMostNode = leftMostNode->left;
+    return ConstIterator(leftMostNode);
   }
 
   const_iterator cend() const
@@ -216,41 +285,63 @@ public:
 
   ConstIterator& operator++()
   {
-    BinaryNode *realCurrentNode = currentNode->parent;
-    if(realCurrentNode->right != nullptr) {
-      realCurrentNode = realCurrentNode->right;
-      while(realCurrentNode->left != nullptr)
-        realCurrentNode = realCurrentNode->left;
-      currentNode = realCurrentNode->left;
+    if(currentNode->parent == nullptr)
+        throw std::out_of_range("Cannot increment end");
+    if(currentNode->right != nullptr) {
+      currentNode = currentNode->right;
+      while(currentNode->left != nullptr)
+        currentNode = currentNode->left;
       return *this;
     }
-    BinaryNode *tmp = realCurrentNode->parent;
-    while(tmp != nullptr && realCurrentNode == tmp->right) {
-      realCurrentNode = tmp;
+    BinaryNode *tmp = currentNode->parent;
+    while(tmp != nullptr && currentNode == tmp->right) {
+      currentNode = tmp;
       tmp = tmp->parent;
     }
-    currentNode = realCurrentNode->left;
+    currentNode = tmp;
     return *this;
   }
 
   ConstIterator operator++(int)
   {
-    throw std::runtime_error("TODO");
+    ConstIterator old(*this);
+    operator++();
+    return old;
   }
 
   ConstIterator& operator--()
   {
-    throw std::runtime_error("TODO");
+    if(currentNode->right == currentNode)
+        throw std::out_of_range("Cannot decrement begin, empty map");
+    if(currentNode->left != nullptr) {
+        currentNode = currentNode->left;
+        while(currentNode->right != nullptr)
+          currentNode = currentNode->right;
+        return *this;
+    }
+    BinaryNode *tmp = currentNode->parent;
+    while(tmp != nullptr && currentNode == tmp->left) {
+      if(tmp->parent == nullptr)
+        throw std::out_of_range("Cannot decrement begin"); //todo
+      currentNode = tmp;
+      tmp = tmp->parent;
+    }
+    currentNode = tmp;
+    return *this;
   }
 
   ConstIterator operator--(int)
   {
-    throw std::runtime_error("TODO");
+    ConstIterator old(*this);
+    operator--();
+    return old;
   }
 
   reference operator*() const
   {
-    return currentNode->parent->data;
+    if(currentNode->right == currentNode)
+      throw std::out_of_range("Cannot dereference end");
+    return currentNode->data;
   }
 
   pointer operator->() const
--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -1,5 +1,5 @@
 #ifndef WIN32
-//#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_DYN_LINK
 #endif
 #define BOOST_TEST_MAIN
 #define BOOST_TEST_MODULE aisdi_maps_tests
